6becbb4f7f5ba04de3f1a1e5a461f3c47837a486
e1turin <pass>
2023-05-10 01:15:02 +0300

    Deleted unused modules

--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/entities/form/model/Settings.kt
+++
@@ -1,8 +0,0 @@
-package io.github.e1turin.output.view.entities.form.model
-
-data class Settings(
-    val functionRangeStart: Double = -1.3,
-    val functionRangeEnd: Double = 3.5,
-    val graphGridStep: Double = 1.0,
-    val graphDensity: Double = 0.025
-)
