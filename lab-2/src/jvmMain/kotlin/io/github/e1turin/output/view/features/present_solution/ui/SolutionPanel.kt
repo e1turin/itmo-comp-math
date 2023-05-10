0116ffd2050b319572c59ebad779d87b591218cb
e1turin <pass>
2023-05-11 01:26:26 +0300

    SimpleIterationSystemSolution stub

--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/SolutionPanel.kt
+++ lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/SolutionPanel.kt
@@ -1,22 +1,10 @@
 package io.github.e1turin.output.view.features.present_solution.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
-import io.github.e1turin.model.domain.equation.nonlinear.method.NewtonSolvingMethod
-import io.github.e1turin.model.domain.equation.nonlinear.solver.IterativeEquationSolver
 import io.github.e1turin.output.view.entities.settings.model.*
-import io.github.e1turin.output.view.features.present_solution.model.SolvingResult
 import io.github.e1turin.output.view.features.present_solution.ui.method.NewtonSolvingMethodSolutionPresenter
-import io.github.e1turin.output.view.shared.lib.std.pretty
-import kotlin.math.abs
+import io.github.e1turin.output.view.features.present_solution.ui.method.SimpleIterationSystemSolutionPresenter
 
 @Composable
 fun SolutionPanel(
@@ -51,7 +39,7 @@ private fun SystemSolutionPanel(
     settings: SystemSettings
 ) {
     when (settings) {
-        is SystemSimpleIterationSettings -> TODO()
+        is SystemSimpleIterationSettings -> SimpleIterationSystemSolutionPresenter(modifier, settings)
 
         DefaultSystemSettings -> {/* Unreachable case */
         }
--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/method/SimpleIterationSystemSolution.kt
+++ lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/method/SimpleIterationSystemSolution.kt
@@ -1,2 +1,33 @@
 package io.github.e1turin.output.view.features.present_solution.ui.method
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import io.github.e1turin.model.domain.equation.nonlinear.system.method.SimpleIterationSystemSolvingMethod
+import io.github.e1turin.output.view.entities.settings.model.SystemSimpleIterationSettings
+import io.github.e1turin.output.view.features.present_solution.model.SolvingResult
+
+@Composable
+fun SimpleIterationSystemSolutionPresenter(
+    modifier: Modifier = Modifier,
+    settings: SystemSimpleIterationSettings
+) {
+    val data by settings.data.subscribeAsState()
+
+    var initialValue = data.initialValue
+
+
+    val result = try {
+        val method = SimpleIterationSystemSolvingMethod(
+            approximationFunction = listOf(
+                { hx -> hx[0] - 1 },
+                { hx -> hx[1] + 1 },
+            )
+        )
+
+    } catch (e: Exception) {
+        SolvingResult.Error(e)
+    }
+
+}
