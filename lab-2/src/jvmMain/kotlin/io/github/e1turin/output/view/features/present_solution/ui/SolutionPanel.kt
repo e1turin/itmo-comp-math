3a86eee1bc34d85ff1571d87f0bcdc949a2f0f0f
e1turin <pass>
2023-05-10 19:42:23 +0300

    Solving with Newton's method stub

--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/SolutionPanel.kt
+++ lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/present_solution/ui/SolutionPanel.kt
@@ -3,6 +3,7 @@ package io.github.e1turin.output.view.features.present_solution.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,16 +25,29 @@ fun SolutionPanel(
 ) {
     val settings = settings as NewtonEquationSettings
     val data by settings.data.subscribeAsState()
+
+    var initialValue: Double = data.initialValue
+
     val solvingResult: SolvingResult = try {
         val method = NewtonSolvingMethod(
             range = data.range,
-            function = data.function!!
+            function = data.function!!,
+//            derivative = {x -> 1 / x}
         )
 
+        initialValue = NewtonSolvingMethod.initialApproximation(
+            range = data.range,
+            function = data.function!!
+//            function = { x -> log(x, 2.7) },
+//            firstDerivative = {x -> 1/x},
+//            secondDerivative = { x -> -1 / x.pow(2.0) }
+        ).also { settings.onInitialValueChange(it) }
+
+        var step = 0
         val solver = IterativeEquationSolver(
             method = method,
-            initialApproximation = data.initialValue,
-            stopCondition = { eps: Double -> abs(eps) < 0.001 }
+            initialApproximation = initialValue,
+            stopCondition = { eps: Double -> abs(eps) < 0.001 || ++step < 10000 }
         )
         val solution = solver.solve(data.function!!)
         SolvingResult.Number(solution)
@@ -41,17 +55,20 @@ fun SolutionPanel(
         SolvingResult.Error(e)
     }
 
-    val outputText: String = when(solvingResult) {
-        is SolvingResult.Error -> solvingResult.exception.message ?: "Error appeared while computing solution"
-        is SolvingResult.Number -> solvingResult.realNumber.pretty()
-        is SolvingResult.Range -> solvingResult.range.pretty()
+    val outputText: String = when (solvingResult) {
+        is SolvingResult.Error -> "⚠ " + (solvingResult.exception.message ?: "Error appeared while computing solution")
+        is SolvingResult.Number -> "x= " + solvingResult.realNumber.pretty()
+        is SolvingResult.Range -> "range: " + solvingResult.range.pretty()
     }
 
 
-    Column(modifier = modifier) {
-        Text("Solution")
-            .also { Spacer(Modifier.size(10.dp)) }
+    SelectionContainer {
+        Column(modifier = modifier) {
+            Text("Solution")
+                .also { Spacer(Modifier.size(10.dp)) }
 
-        Text(outputText)
+            Text("initial value: ${initialValue.pretty()}")
+            Text(outputText)
+        }
     }
 }
