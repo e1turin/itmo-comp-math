ec5afdd012f47215b8e09c25c00a5b82c81fe13a
e1turin <pass>
2023-05-14 01:34:53 +0300

    Included serialization and util func for file system interop

--- lab-2/build.gradle.kts
+++ lab-2/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.8.0"
     id("org.jetbrains.compose")
 }
 
@@ -25,6 +26,9 @@ kotlin {
                 implementation(compose.desktop.currentOs)
                 implementation("com.arkivanov.decompose:decompose:2.0.0-alpha-02")
                 implementation("com.arkivanov.decompose:extensions-compose-jetbrains:2.0.0-alpha-02")
+                implementation("com.darkrockstudios:mpfilepicker:1.1.0")
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
             }
         }
         val jvmTest by getting
