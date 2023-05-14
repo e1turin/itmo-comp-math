613afbd496525d540224f7d2d0f3a38c3aa5f3b8
e1turin <pass>
2023-05-15 01:10:20 +0300

    New utility functions for floating point types

--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/shared/lib/std/range.kt
+++ lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/shared/lib/std/range.kt
@@ -1,7 +1,5 @@
 package io.github.e1turin.output.view.shared.lib.std
 
-val ClosedRange<Float>.length: Float get() = endInclusive - start
-val ClosedRange<Double>.length: Double get() = endInclusive - start
 
 fun ClosedRange<Double>.toFloatRange(): ClosedFloatingPointRange<Float> =
     start.toFloat()..endInclusive.toFloat()
@@ -9,10 +7,12 @@ fun ClosedRange<Double>.toFloatRange(): ClosedFloatingPointRange<Float> =
 fun ClosedRange<Float>.toDoubleRange(): ClosedRange<Double> =
     start.toDouble()..endInclusive.toDouble()
 
-
 fun List<ClosedRange<Double>>.toFloatRanges(): List<ClosedFloatingPointRange<Float>> =
     this.map { it.toFloatRange() }
 
+
+/* Float */
+val ClosedRange<Float>.length: Float get() = endInclusive - start
 
 fun ClosedRange<Float>.slideToLowestBy(step: Float): ClosedFloatingPointRange<Float> =
     (start - step)..(endInclusive - step)
@@ -35,6 +35,22 @@ inline infix fun ClosedRange<Float>.unsteadyStep(step: Float) = object : Iterato
     override fun next(): Float =
         current.also { current += step }
 }
+
+
+/* Double */
+val ClosedRange<Double>.length: Double get() = endInclusive - start
+
+fun ClosedRange<Double>.slideToLowestBy(step: Double): ClosedFloatingPointRange<Double> =
+    (start - step)..(endInclusive - step)
+
+fun ClosedRange<Double>.slideToHighestBy(step: Double): ClosedFloatingPointRange<Double> =
+    (start + step)..(endInclusive + step)
+
+fun ClosedRange<Double>.shrinkBy(step: Double): ClosedFloatingPointRange<Double> =
+    (start + step)..(endInclusive - step)
+
+fun ClosedRange<Double>.stretchBy(step: Double): ClosedFloatingPointRange<Double> =
+    (start - step)..(endInclusive + step)
 
 inline infix fun ClosedRange<Double>.unsteadyStep(step: Double) = object : Iterator<Double> {
     private var current = start
