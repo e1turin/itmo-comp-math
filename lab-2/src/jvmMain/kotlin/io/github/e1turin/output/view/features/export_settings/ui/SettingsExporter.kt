c46a035a4cfe0c917a34e59e1a3de344793646a1
e1turin <pass>
2023-05-15 01:08:49 +0300

    Fix settings exporter behaviour

--- lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/export_settings/ui/SettingsExporter.kt
+++ lab-2/src/jvmMain/kotlin/io/github/e1turin/output/view/features/export_settings/ui/SettingsExporter.kt
@@ -1,9 +1,10 @@
 package io.github.e1turin.output.view.features.export_settings.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import io.github.e1turin.output.view.entities.file_system.model.FileSystemAdapter
 import io.github.e1turin.output.view.entities.file_system.ui.FileSelector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 
@@ -12,28 +13,29 @@ fun SettingsExporter(
     jsonData: String,
     onComplete: () -> Unit = {}
 ) {
-    val coroutineScope = rememberCoroutineScope()
 
     FileSelector(
         show = true,
-        onFileSelected = { file ->
-            val filePath = file?.path
+    ) { file ->
 
-            if (filePath != null) {
-                coroutineScope.launch {
-                    try {
-                        supervisorScope {
-                            FileSystemAdapter.saveToFile(filePath, jsonData)
-                        }
-                    } catch (e: Exception) {
-                        println(e)
+        val filePath = file?.path
+
+        if (filePath != null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    supervisorScope {
+                        FileSystemAdapter.saveToFile(filePath, jsonData)
                     }
+                } catch (e: Exception) {
+                    println("[SettingsExporter.kt]exception appeared: $e")
                 }
             }
-
-            onComplete()
+        } else {
+            println("[SettingsExporter.kt]export path is null")
         }
-    )
+
+        onComplete()
+    }
 
 
 }
