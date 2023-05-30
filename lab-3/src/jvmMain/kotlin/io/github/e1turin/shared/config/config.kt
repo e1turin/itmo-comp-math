bbf17d150b0b4d2460a3576e5480e6ae9c6604ba
e1turin <pass>
2023-05-30 14:11:19 +0300

    New example and description fix

--- lab-3/src/jvmMain/kotlin/io/github/e1turin/shared/config/config.kt
+++ lab-3/src/jvmMain/kotlin/io/github/e1turin/shared/config/config.kt
@@ -1,12 +1,14 @@
 package io.github.e1turin.shared.config
 
 import kotlin.math.log
+import kotlin.math.sin
 
 val functionWithLabelStore: Map<String, (Double) -> Double> = mapOf(
     "x" to { x -> x },
     "x^2" to { x -> x * x },
     "ln(x)" to { x -> log(x, kotlin.math.E) },
-    "1/x" to { x -> 1 / x }
+    "1/x" to { x -> 1 / x },
+    "sin(x)/x" to { x -> sin(x) / x }
 )
 
 const val maxNIteration = 20
