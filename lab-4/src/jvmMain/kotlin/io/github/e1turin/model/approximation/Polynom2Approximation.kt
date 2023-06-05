b409fed6b83172398b23a74c175d890341b9548b
e1turin <pass>
2023-06-05 13:59:36 +0300

    Simple logic of approximation for polynomial up to 3 power and exponential.

---
+++ lab-4/src/jvmMain/kotlin/io/github/e1turin/model/approximation/Polynom2Approximation.kt
@@ -0,0 +1,58 @@
+package io.github.e1turin.model.approximation
+
+import io.github.e1turin.model.matrix.solveSLE
+import io.github.e1turin.model.matrix.toMatrix
+import kotlin.math.pow
+
+class Polynom2Approximation : Approximation {
+    private var a0: Double? = null
+    private var a1: Double? = null
+    private var a2: Double? = null
+
+    override val function: (Double) -> Double
+        get() {
+            checkState()
+            return { x -> a0!!  + a1!! * x + a2!! * x.pow(2) }
+        }
+
+    override val params: List<Double>
+        get() {
+            checkState()
+            return listOf(a0!!, a1!!, a2!!)
+        }
+
+    /**
+     * @throws ArithmeticException if regression can not be completed due to zero value of matrix determinant.
+     */
+    override fun fit(x: DoubleArray, y: DoubleArray) {
+        require(x.size == y.size) { "x and y arrays must have equal amount of elements" }
+
+        val n = x.size.toDouble()
+        val sx = x.sum()
+        val sy = y.sum()
+        val sx2 = x.reduce { acc, d -> acc + d * d }
+        val sxy = x.reduceIndexed { idx, acc, d -> acc + d * y[idx]}
+        val sx3 = x.reduce { acc, d -> acc + d.pow(3) }
+        val sx2y = x.reduceIndexed { idx, acc, d -> acc + d * d * y[idx] }
+        val sx4 = x.reduce { acc, d -> acc + d.pow(4) }
+
+        val matrix = arrayOf(
+            doubleArrayOf(n, sx, sx2),
+            doubleArrayOf(sx, sx2, sx3),
+            doubleArrayOf(sx2, sx3, sx4)
+        ).toMatrix()
+
+        val vector = doubleArrayOf(sy, sxy, sx2y)
+
+        val solution = matrix.solveSLE(vector) ?: throw ArithmeticException("Can not solve system of equations")
+        a0 = solution[0]
+        a1 = solution[1]
+        a2 = solution[2]
+    }
+
+    override fun predict(x: DoubleArray): DoubleArray {
+        return DoubleArray(x.size) { function(x[it]) }
+    }
+
+    private fun checkState() = check(a0 != null && a1 != null && a2 != null) { "Model must be fitted before" }
+}
---
+++ lab-4/src/jvmMain/kotlin/io/github/e1turin/model/approximation/Polynom3Approximation.kt
@@ -0,0 +1,63 @@
+package io.github.e1turin.model.approximation
+
+import io.github.e1turin.model.matrix.solveSLE
+import io.github.e1turin.model.matrix.toMatrix
+import kotlin.math.pow
+
+class Polynom3Approximation : Approximation {
+    private var a0: Double? = null
+    private var a1: Double? = null
+    private var a2: Double? = null
+    private var a3: Double? = null
+
+    override val function: (Double) -> Double
+        get() {
+            checkState()
+            return { x -> a0!! + a1!! * x + a2!! * x.pow(2) + a3!! * x.pow(3) }
+        }
+
+    override val params: List<Double>
+        get() {
+            checkState()
+            return listOf(a0!!, a1!!, a2!!, a3!!)
+        }
+
+    /**
+     * @throws ArithmeticException if regression can not be completed due to zero value of matrix determinant.
+     */
+    override fun fit(x: DoubleArray, y: DoubleArray) {
+        require(x.size == y.size) { "x and y arrays must have equal amount of elements" }
+
+        val n = x.size.toDouble()
+        val sx = x.sum()
+        val sy = y.sum()
+        val sx2 = x.reduce { acc, d -> acc + d * d }
+        val sxy = x.reduceIndexed { idx, acc, d -> acc + d * y[idx]}
+        val sx3 = x.reduce { acc, d -> acc + d.pow(3) }
+        val sx2y = x.reduceIndexed { idx, acc, d -> acc + d * d * y[idx] }
+        val sx4 = x.reduce { acc, d -> acc + d.pow(4) }
+        val sx3y = x.reduceIndexed { idx, acc, d -> acc + d.pow(3) * y[idx] }
+        val sx5 = x.reduce { acc, d -> acc + d.pow(5) }
+        val sx6 = x.reduce { acc, d -> acc + d.pow(6) }
+
+        val matrix = arrayOf(
+            doubleArrayOf(n, sx, sx2, sx3),
+            doubleArrayOf(sx, sx2, sx3, sx4),
+            doubleArrayOf(sx2, sx3, sx4, sx5),
+            doubleArrayOf(sx3, sx4, sx5, sx6)
+        ).toMatrix()
+
+        val vector = doubleArrayOf(sy, sxy, sx2y, sx3y)
+
+        val solution = matrix.solveSLE(vector) ?: throw ArithmeticException("Can not solve system of equations")
+        a0 = solution[0]
+        a1 = solution[1]
+        a2 = solution[2]
+    }
+
+    override fun predict(x: DoubleArray): DoubleArray {
+        return DoubleArray(x.size) { function(x[it]) }
+    }
+
+    private fun checkState() = check(a0 != null && a1 != null && a2 != null && a3 != null) { "Model must be fitted before" }
+}
